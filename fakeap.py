d0f88046b6f714b3224428917b32e88e9ca92afe
Austinstevesk <austinstevesk@gmail.com>
2021-03-15 18:38:52 +0300

    Added a single fakeap

--- fakeap.py
+++ fakeap.py
@@ -17,3 +17,23 @@ frame = RadioTap()/dot11/beacon/essid
 # send the frame in layer 2 every 100 milliseconds forever
 # using the `iface` interface
 sendp(frame, inter=0.1, iface=iface, loop=1)
+
+
+"""The above code does the following:
+
+We generate a random MAC address as well as setting a name of our access point we want to create and then we create a 802.11 frame, 
+the fields are:
+
+    type=0:  indicates that it is a management frame.
+    subtype=8:  indicates that this management frame is a beacon frame.
+    addr1: refers to the destination MAC address, in other words, the receiver's MAC address, we use the broadcast address here 
+    ("ff:ff:ff:ff:ff:ff"), 
+    if you want this fake access point to appear only in a target device, you can use the target's MAC address.
+    addr2: source MAC address, the sender's MAC address.
+    addr3: the MAC address of the access point.
+
+So we should use the same MAC address of addr2 and addr3, that's because the sender is the access point!
+
+We create our beacon frame with ssid infos and then stack them all together and send them using Scapy's sendp() function.
+
+After we setup our interface into monitor mode and execute the script we can see something like: [Test] as a BSSID"""
